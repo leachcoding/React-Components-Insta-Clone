9bf788c1d5555a14484987ee592a81f0a763b5df
Jay Leach <leachcoding@gmail.com>
2020-05-14 16:26:08 -0400

    MVP met

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -3,14 +3,19 @@ import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
 import "./Comment.css";
+import dummyData from '../../dummy-data';
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const [comment, setComment] = useState(props.comments);
+  console.log(comment, 'comment useState')
   return (
     <div>
       {/* map through the comments data and return the Comment component */}
-      <CommentInput />
+      {comment.map(comment => {
+        return <Comment comment={comment}/>
+      })}
+      <CommentInput comment={comment}/>
     </div>
   );
 };
